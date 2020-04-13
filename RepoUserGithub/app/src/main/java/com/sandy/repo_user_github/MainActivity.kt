a4a35f2ed3fc33b98b96982fd7db18463171f05a
Sandy Umar Yuda <sandiyuda@Sandys-MacBook-Air.local>
2020-04-13 18:33:54 +0700

    first commit

---
+++ RepoUserGithub/app/src/main/java/com/sandy/repo_user_github/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.sandy.repo_user_github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
